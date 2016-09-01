b2522ada65b9facff898220c5f3f8849f5651ec0
blokkinen <viktoria_blok@hotmail.com>
2016-09-01 13:56:45 +0200

    deleted the code I got from Suss and added another one

--- config.rb
+++ config.rb
@@ -18,6 +18,7 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
+set :relative_links, true
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -39,7 +40,6 @@ end
 configure :build do
   # Minify CSS on build
   # activate :minify_css
-  activate :relative_assets
   activate :build_cleaner
 end
   # Minify Javascript on build
